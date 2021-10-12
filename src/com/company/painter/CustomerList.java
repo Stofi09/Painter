51806daf6b8d4a1e720ec12fb05b891863d3e9d3
Kris <kristof.solak@gmail.com>
2021-10-12 12:22:33 +0200

    init

--- src/com/company/painter/CustomerList.java
+++ src/com/company/painter/CustomerList.java
@@ -1,5 +1,22 @@
 package com.company.painter;
 
+import java.util.ArrayList;
+
 public class CustomerList {
 
+
+    private static ArrayList<Customer> customers = new ArrayList<>();
+    private static CustomerList ListInstance = null;
+
+    private CustomerList(){}
+
+    public static void addCustomer(Customer customer){
+        if (ListInstance == null){
+            ListInstance = new CustomerList();
+        }
+        customers.add(customer);
+    }
+    public static ArrayList<Customer> getList(){
+        return customers;
+    }
 }
