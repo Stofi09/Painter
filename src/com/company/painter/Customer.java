91dc804688549470c637c2c852c4e3496f0ca851
Kris <kristof.solak@gmail.com>
2021-10-12 12:08:21 +0200

    Getters and variables added.

--- src/com/company/painter/Customer.java
+++ src/com/company/painter/Customer.java
@@ -1,4 +1,25 @@
 package com.company.painter;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Customer {
+
+    private String name;
+    private ArrayList<Order> orders = new ArrayList<>();
+
+    public Customer (String name){
+        this.name = name;
+    }
+
+    public void addOrder(Order order){
+        this.orders.add(order);
+    }
+    public ArrayList<Order> listOders(){
+        return this.orders;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
