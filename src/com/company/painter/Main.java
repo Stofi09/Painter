c47816775890f1d44f7817f8ff9b294e8d3cc42c
Kris <kristof.solak@gmail.com>
2021-10-13 11:09:46 +0200

    Moved the training stuff to a new class

--- src/com/company/painter/Main.java
+++ src/com/company/painter/Main.java
@@ -44,58 +44,7 @@ public class Main {
         CustomerList.addCustomer(customer);
         System.out.println("Thank you " + customer.getName() + ", your order will be completed shortly.");
         Thread.sleep(3000);
-        /*for (int i = 0; i < 30; i++){
-            System.out.println("Computing");
-        }
-        int[] numbers =     {1,2,3,4,5,6,7,8,9,10};
-        int sumOfTheNumbers = 0;
-        for(int j : numbers){
-            sumOfTheNumbers += j;
-        }
-        System.out.println("That is the sum: "+sumOfTheNumbers);
-        System.out.println("Your order is ready, the paint will cost: " + order.getCost() + "£");
-        */
 
-        /* Training stuffs
-        if (customer.getType().name().equalsIgnoreCase("SILVER")){
-            System.out.println("You have to spend 5000 more to become a GOLD level customer");
-        }else if (customer.getType().name().equalsIgnoreCase("GOLD")){
-            System.out.println("THANK YOU! Because of you, we can all go to the Bahamas!");
-        }else {
-            System.out.println("You have to spend more man!");
-        }
-
-        String something = switch(customer.getType().name()){
-            case "BRONZE" -> "just a bronze";
-            case "SILVER" -> "Not bad";
-            case "GOLD" -> "Yeehah!";
-            default -> "Nothing";
-        };
-
-
-        System.out.println("Something: "+""+something);
-
-        boolean playAgain = true;
-        int count = 0;
-        while(playAgain){
-            if(count > 10){
-                playAgain = false;
-            }
-            System.out.println(count++);
-        }
-        count = 0;
-        do{
-            if(count>10){
-                playAgain = true;
-            }
-        }while (!playAgain);
-
-        for(int i = 0; i < customer.listOders().size(); i++){
-            System.out.println(customer.listOders().get(i));
-        }
-        */
-        // Best practice: Known amount -> For loop
-        // Not known -> While
     }
 
 }
